0a2afa6135222fa2dc996302eb29d75608d45c7f
Pavel Shvetsov <shvedusa@gmail.com>
2021-05-25 19:56:29 -0400

    initial commit

---
+++ src/test/java/BranchingPractice.java
@@ -0,0 +1,12 @@
+public class BranchingPractice {
+    public static void main(String[] args) {
+
+        //created new project
+        //enabled version control
+        //added .gitignore with .idea, target folder and .iml file
+        //shared project on github
+        //reviewing on new branch
+
+    }
+
+}
